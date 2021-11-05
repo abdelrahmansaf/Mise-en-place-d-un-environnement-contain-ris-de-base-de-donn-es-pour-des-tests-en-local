0497eab0c1468109fe07d29b737972286ec119cf
abdelrahmansaf <77361942+abdelrahmansaf@users.noreply.github.com>
2021-11-05 13:34:53 +0100

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# FROM mysql:latest as builder
+# RUN ["sed", "-i", "s/exec \"$@\"/echo \"not running $@\"/", "/usr/local/bin/docker-entrypoint.sh"]
+# ENV MYSQL_ROOT_PASSWORD root
+# COPY setup.sql /docker-entrypoint-initdb.d/
+# RUN ["/usr/local/bin/docker-entrypoint.sh", "mysqld", "--datadir", "/initialized-db"]
+# FROM mysql:latest
+# COPY --from=builder /initialized-db /var/lib/mysql
+
+FROM mysql:latest
+COPY setup.sql /docker-entrypoint-initdb.d/
+EXPOSE 3306
---
+++ setup.sql
@@ -0,0 +1,93 @@
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`user` (
+
+  `username` VARCHAR(16) NOT NULL,
+  `email` VARCHAR(255) NULL,
+  `password` VARCHAR(32) NOT NULL,
+  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `balance` INT NULL DEFAULT 0,
+  `id` INT PRIMARY KEY AUTOINCREMENT,
+  PRIMARY KEY (`id`)
+  );
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`category`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`category` (
+  `name` VARCHAR(255) NOT NULL,
+  `id` INT NOT NULL,
+  PRIMARY KEY (`id`)
+  );
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`products`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`products` (
+  `id` INT PRIMARY KEY AUTOINCREMENT,
+  `owner` INT NULL,
+  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `category` INT NOT NULL,
+  `price` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `quantity` INT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  INDEX `owner_idx` (`owner` ASC) VISIBLE,
+  INDEX `category_idx` (`category` ASC) VISIBLE,
+  CONSTRAINT `owner`
+    FOREIGN KEY (`owner`)
+    REFERENCES `mydb`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `category`
+    FOREIGN KEY (`category`)
+    REFERENCES `mydb`.`category` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`history`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`history` (
+  `id` INT NOT NULL,
+  `seller` INT NULL,
+  `buyer` INT NULL,
+  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `facture` INT NULL,
+  `product` INT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `buyer_idx` (`buyer` ASC) VISIBLE,
+  INDEX `product_idx` (`product` ASC) VISIBLE,
+  INDEX `seller_idx` (`seller` ASC) VISIBLE,
+  CONSTRAINT `seller`
+    FOREIGN KEY (`seller`)
+    REFERENCES `mydb`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `buyer`
+    FOREIGN KEY (`buyer`)
+    REFERENCES `mydb`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `product`
+    FOREIGN KEY (`product`)
+    REFERENCES `mydb`.`products` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
